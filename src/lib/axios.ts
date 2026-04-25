1d79880dc99c79e3dcfa404bbb40ce04c22d9c09
yassin-x <yassin.ibrahim.hamed@gmail.com>
2026-04-25 19:24:17 +0300

    ..

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -19,8 +19,7 @@ export interface ApiError {
 }
 
 export const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api/v1",
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
